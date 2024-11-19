14ab83f9bd45f083fe276516000d4cb175960b5b
Nicolas <nico.hhara@gmail.com>
2024-11-19 09:16:54 -0300

    musica sem loop, e animacao botao

--- GameOver/GameOver.js
+++ GameOver/GameOver.js
@@ -1,7 +1,28 @@
 var timer, sound;
 sound = new Howl({
-    src: ['final.mp3']
+    src: ['finalRuim.mp3'],
+    autoplay: true,
+    loop: false,
+    volume: 1,
 });
-setInterval(function(){
-sound.play();
-},1000);
+setTimeout(function(){
+    sound.play();
+    },100);
+
+anime.timeline({loop: false})
+    .add({
+      targets: '.ml16 .word3',
+      scale: [14,1],
+      opacity: [0,1],
+      easing: "easeOutCirc",
+      duration: 800,
+      delay: (el, i) => 800 * i
+    }).add({
+        targets: '.ml16 .word4',
+        scale: [14,1],
+        opacity: [0,1],
+        easing: "easeOutCirc",
+        duration: 800,
+        delay: (el, i) => 800 * i
+      })
+
