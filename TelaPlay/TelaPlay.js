16701812a1b56b2bff15716a75408ef2ded205c0
Nicolas <nico.hhara@gmail.com>
2024-11-21 09:10:39 -0300

    sdadasd

--- TelaPlay/TelaPlay.js
+++ TelaPlay/TelaPlay.js
@@ -8,21 +8,3 @@ sound = new Howl({
 setTimeout(function(){
     sound.play();
     },100);
-
-var textWrapper = document.querySelector('.ml9 .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-anime.timeline({loop: true})
-  .add({
-    targets: '.ml9 .letter',
-    scale: [0, 1],
-    duration: 1500,
-    elasticity: 600,
-    delay: (el, i) => 45 * (i+1)
-  }).add({
-    targets: '.ml9',
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000
-  });
