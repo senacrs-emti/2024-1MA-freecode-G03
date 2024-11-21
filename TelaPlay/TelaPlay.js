bb5ca9b674b8ec14b03924c7c60497e9b0fc7b33
Nicolas <nico.hhara@gmail.com>
2024-11-21 08:54:08 -0300

    asdasdad

---
+++ TelaPlay/TelaPlay.js
@@ -0,0 +1,28 @@
+var timer, sound;
+sound = new Howl({
+    src: ['InicioPlay.mp3'],
+    autoplay: true,
+    loop: true,
+    volume: 1,
+});
+setTimeout(function(){
+    sound.play();
+    },100);
+
+var textWrapper = document.querySelector('.ml9 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml9 .letter',
+    scale: [0, 1],
+    duration: 1500,
+    elasticity: 600,
+    delay: (el, i) => 45 * (i+1)
+  }).add({
+    targets: '.ml9',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
