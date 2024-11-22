ea1b12316a4d2cb2509ea0729bb81a794548b1e0
Nicolas <nico.hhara@gmail.com>
2024-11-22 11:08:28 -0300

    Animacao titulo

--- TelaPlay/TelaPlay.js
+++ TelaPlay/TelaPlay.js
@@ -1,10 +1,19 @@
-var timer, sound;
-sound = new Howl({
-    src: ['InicioPlay.mp3'],
-    autoplay: true,
-    loop: true,
-    volume: 1,
-});
-setTimeout(function(){
-    sound.play();
-    },100);
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml9 .Letras');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='Letras'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml9 .Letras',
+    scale: [0, 1],
+    duration: 2500,
+    elasticity: 600,
+    delay: (el, i) => 45 * (i+1)
+  }).add({
+    targets: '.ml9',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
